785362495eab61cce8a3f5b65b8950bd2217fd8f
hyuncello <jeremykim95@gmail.com>
2021-04-30 23:47:14 +0900

    0430_C_++_Lec2_example

---
+++ Programming/C++/Lec2_namespace/HelloHyuncello.cpp
@@ -0,0 +1,14 @@
+#include "HelloHyuncello.h"
+#include "HiHyuncello.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // 1. Namespace Example
+    hello::SayHelloExample();
+    hi::SayHelloExample();
+
+    return 0;
+}
